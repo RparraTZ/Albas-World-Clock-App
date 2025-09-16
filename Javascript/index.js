bdf6561efb6bd04fc1114b0434d6b2e78350170f
RparraTZ <rparratellez@gmail.com>
2025-09-15 19:31:28 -0700

    Made functions for data for LA and Sydney
    Made the functions for Local LA time and Australia using moment and moment tz. Also set interval for updating display every second.

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,23 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".Date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".Time");
-losAngelesDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = moment().format(
-  "h:mm:ss [<small>] A[</small]"
-);
+function losAngelesGetTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".Date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".Time");
+  losAngelesDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = moment().format(
+    "h:mm:ss [<small>] A[</small>]"
+  );
+}
+function sydneyGetTime() {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".Date");
+  let sydneyTimeElement = sydneyElement.querySelector(".Time");
+  sydneyDateElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("dddd, MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("h:mm:ss [<small>] A[</small>]");
+}
+
+setInterval(losAngelesGetTime, 1000);
+setInterval(sydneyGetTime, 1000);
