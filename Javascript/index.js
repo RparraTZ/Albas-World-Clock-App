54cf3a58470a78694f64ca3ae1d14cc213e7ed5d
RparraTZ <rparratellez@gmail.com>
2025-09-15 21:08:40 -0700

    Added moments data for select element
    Added values to select for moment to retrieve data and targeted HTML from JS to display said data.

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,23 +1,46 @@
 function losAngelesGetTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".Date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".Time");
-  losAngelesDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = moment().format(
-    "h:mm:ss [<small>] A[</small>]"
-  );
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".Date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".Time");
+    losAngelesDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = moment().format(
+      "h:mm:ss [<small>] A[</small>]"
+    );
+  }
 }
 function sydneyGetTime() {
   let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".Date");
-  let sydneyTimeElement = sydneyElement.querySelector(".Time");
-  sydneyDateElement.innerHTML = moment()
-    .tz("Australia/Sydney")
-    .format("dddd, MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = moment()
-    .tz("Australia/Sydney")
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".Date");
+    let sydneyTimeElement = sydneyElement.querySelector(".Time");
+    sydneyDateElement.innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("dddd, MMMM Do YYYY");
+    sydneyTimeElement.innerHTML = moment()
+      .tz("Australia/Sydney")
+      .format("h:mm:ss [<small>] A[</small>]");
+  }
+}
+function getCityData(event) {
+  let selectedCity = event.target.value;
+  let cityName = selectedCity.replace("_", " ").split("/")[1];
+  let citiesContainerElement = document.getElementById("CitiesContainer");
+  let selectedCityDate = moment().tz(selectedCity).format("dddd, MMMM Do YYYY");
+  let selectedCityTime = moment()
+    .tz(selectedCity)
     .format("h:mm:ss [<small>] A[</small>]");
+  citiesContainerElement.innerHTML = `
+<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="Date">${selectedCityDate}</div>
+          </div>
+          <div class="Time">${selectedCityTime}</div>
+        </div>`;
 }
 
 setInterval(losAngelesGetTime, 1000);
 setInterval(sydneyGetTime, 1000);
+let selectedcity = document.getElementById("city-selection");
+selectedcity.addEventListener("change", getCityData);
