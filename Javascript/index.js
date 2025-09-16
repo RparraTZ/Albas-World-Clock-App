75f134d3a08c932ca0087341a3cf4252c673c2a1
RparraTZ <rparratellez@gmail.com>
2025-09-15 18:55:05 -0700

    change vague Ids and classes for more specific ones

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,7 @@
-
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".Date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".Time");
+losAngelesDateElement.innerHTML = moment().format("dddd, MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = moment().format(
+  "h:mm:ss [<small>] A[</small]"
+);
